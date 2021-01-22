fa61efbabcf506cb4aa6b6b89a82f3133656ee73
kyb <js@szpilewski.com>
2021-01-22 13:04:04 +0100

    scripties

--- voldown.sh
+++ voldown.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 pactl set-sink-volume @DEFAULT_SINK@ -1%
-beep
+#beep
 #notify-send -t 250 "`pamixer --get-volume`%"
 
--- volup.sh
+++ volup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 pactl set-sink-volume @DEFAULT_SINK@ +1%
-beep
+#beep
 #notify-send -t 250 "`pamixer --get-volume`%"
